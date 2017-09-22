18182a4904b02f581d5b00e9ee67741fd7177a9e
builder-247 <olaakkonen@gmail.com>
2017-09-22 16:47:44 +0300

    fix

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
 achievements: require(__dirname + '/build/achievements.json'),
 game_types: require(__dirname + '/build/game_types.json'),
+lobby_names: require(__dirname + '/build/lobby_names.json'),
 pet_xp: require(__dirname + '/build/pet_xp.json')
 };
