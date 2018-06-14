175d93bc02ecc80148d7ff6e5440bcc59f7af378
builder_247 <olaakkonen@gmail.com>
2018-06-14 20:22:12 +0300

    Changed game_types
    All minigame naming convention related data is now stored in game_types.json

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ module.exports = {
 achievements: require(__dirname + '/build/achievements.json'),
 game_types: require(__dirname + '/build/game_types.json'),
 languages: require(__dirname + '/build/languages.json'),
-lobby_names: require(__dirname + '/build/lobby_names.json'),
 pet_xp: require(__dirname + '/build/pet_xp.json')
 };
