771c72b792a7c3e4cb7c271687fe754093bb06be
builder-247 <olaakkonen@gmail.com>
2018-10-05 13:04:48 +0300

    New build

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ achievements_extended: require(__dirname + '/build/achievements_extended.json'),
 challenges: require(__dirname + '/build/challenges.json'),
 game_types: require(__dirname + '/build/game_types.json'),
 languages: require(__dirname + '/build/languages.json'),
+maps: require(__dirname + '/build/maps.json'),
 pet_xp: require(__dirname + '/build/pet_xp.json'),
 quests: require(__dirname + '/build/quests.json')
 };
